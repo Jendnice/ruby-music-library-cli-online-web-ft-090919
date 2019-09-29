a0498d3cd53d9145f33f0bddd08ea731d3066178
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-29 13:16:50 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -93,7 +93,7 @@ class MusicLibraryController
      input = gets.strip.to_i
      
      if (1..Song.all.length).include?(input+1)
-       song_choice = list_songs[input+1]
+       song = list_songs[input+1]
        puts "Playing #{song.name} by #{song.artist.name}"
       end 
    end 
