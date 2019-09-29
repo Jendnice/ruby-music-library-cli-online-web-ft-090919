d22471f91619cfede01369715d627d58eb67e974
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-29 13:11:48 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -40,10 +40,9 @@ class MusicLibraryController
   songs_sorted_by_name = Song.all.sort_by do |song|
       song.name
     end
-  list_songs = songs_sorted_by_name.each.with_index(1) do |song, index|
+  songs_sorted_by_name.each.with_index(1) do |song, index|
       puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
-    list_songs 
   end 
   
   
